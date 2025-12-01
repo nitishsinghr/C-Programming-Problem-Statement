8df81be1a61a664ba4d7a949633b54a48f099f92
NITISH SINGH <151989612+nitishsinghr@users.noreply.github.com>
2025-12-01 14:37:58 +0530

    Fix loop structure and clean up code
    Removed extraneous lines and corrected loop structure.

--- pattern.1c.cpp
+++ pattern.1c.cpp
@@ -6,9 +6,6 @@ int main()
 	printf(" * ");
 	for(int i=1;j<=n;j++)
 	printf(" * ");tttatttr5866ttgoi m
-	ghjo8yui 
-	gjhjgh
-	fgdfjuu236xcgni9tush d nhhgjut8ugdm55555afrgeetrtniunitish singh nitish singh \s
 	
 	
 	
